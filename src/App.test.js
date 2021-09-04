3afaa1ba7a17aaa8b8eff5d6fa33df6e204f5b17
yeon <dalcon10280@gmail.com>
2021-09-05 04:05:47 +0900

    feat: Rendering the Movies

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import { queryByText, render, waitFor, screen } from '@testing-library/react';
+import MockAdapter from "axios-mock-adapter";
+import { moviesMock } from "./moviesMock";
+import axios from "axios";
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App 컴포넌트", () => {
+
+  const fetchMoviesMock = new MockAdapter(axios);
+  const movieApi = 'https://yts-proxy.now.sh/list_movies.json/sort_by=rating'
+
+  it("컴포넌트 시작시 로딩이 먼저 나온 후 영화 목록이 나온다.", async () => {
+    fetchMoviesMock.onGet(movieApi).reply(200, moviesMock);
+    const { getByText } = render(<App />);
+    const loadingElement = getByText(/Loading.../i);
+    expect(loadingElement).not.toBeNull();
+    expect(loadingElement).toBeInstanceOf(HTMLDivElement);
+    
+    // await waitFor(() => );
+    // expect(loadingElement).not.toBeInTheDocument();
+  });
 });
+
